955547e731fbcd3e206d863f19ff1194816b9034
unknown <Fanor@FDN-WS-023166.acad.dvuadmin.net>
2018-01-27 09:06:42 -0300

    01

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const app = express();
 
-    app.get('/hello', (req, res) => {
-        console.log('hello from the other side');
-        res.send('hello world');
+    app.get('/hello/:id', (req, res) => {
+        console.log(req.params);
+        res.send('Hello ' + req.query.name + ' ' + req.params.id);
     })
 
 app.listen(3000)
